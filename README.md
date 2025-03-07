27b1d31f0f07f694b8d32f30a5e10a652af6fd80
JKozina02 <jedrek.kozina02@gmail.com>
2025-03-07 14:58:35 +0100

    feat: add creating user and login in

--- README.md
+++ README.md
@@ -1,16 +1,12 @@
 # GymAppServer
 Server with api for GymApp 
 
-# Starting server
+## Starting server
 
-1: In your repo create venv with:
-    python -m venv .venv
+1. In your repo create venv with: `python -m venv .venv`
 
-2: Lunch your venv with:
-    .venv\Scripts\Activate.ps1
+2. Lunch your venv with: `.venv\Scripts\Activate.ps1`
 
-3: Install fastapi with:
-    pip install "fastapi[standard]"
+3. Install fastapi with: `pip install "fastapi[standard]"`
 
-4: Start server with:
-    fastapi dev main.py
+4. Start server with: `fastapi dev main.py`
--- main.py
+++ main.py
@@ -6,14 +6,31 @@ import data as dt
 app = FastAPI()
 
 Items: List[dt.Exercise] = dt.exercises
+Users: List[dt.User] = dt.users
 
-@app.get("/")
-def read_root():
-    return {"Item" : Items[0]}
+#User login and veryfication
 
+#Create new user
+@app.post("/create-user/")
+def create_user(email: str, username: str, password: str):
+    try:
+        dt.createUser(username=username, email=email, password=password)
+        return {"status-code" : 201, "description": 'User created succesfully'}
+    except ValueError as a:
+        raise HTTPException(status_code=401, detail=a)
+    
+@app.post("/login-user/")
+def login_user(email: str, password: str):
+    if dt.loginUser(email, password):
+        return {"status-code" : 202, "description": 'Logged in'}
+    else:
+        raise HTTPException(status_code=402, detail="Wrong email or password")
+    
+#Exercise 
 @app.get("/exercise/{exercise_id}", response_model=dt.Exercise)
 def read_exercise(exercise_id: int):
     try:
         return Items[exercise_id]
     except IndexError:
         raise HTTPException(status_code=404, detail="Exercise not found")
+    
